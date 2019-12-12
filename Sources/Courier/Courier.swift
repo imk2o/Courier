c3cb2a021171d559d3f017178428284c59dd0811
imk2o <imk2o@icloud.com>
2019-12-12 12:36:08 +0900

    Initial Commit

---
+++ Sources/Courier/Courier.swift
@@ -0,0 +1,3 @@
+struct Courier {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import CourierTests
+
+var tests = [XCTestCaseEntry]()
+tests += CourierTests.allTests()
+XCTMain(tests)
